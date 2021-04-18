bd20e7aa5b806832f8ed24bc7ed9267a7a8a1dff
vjasieg <vjsaieg>
2021-04-18 14:50:24 +0200

    categories added (categories.json)

--- models/product.js
+++ models/product.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const productSchema = mongoose.Schema({
     title: {type: String, required: true},
     desc: {type: String, required: true},
+    category: {type: String, required: true},
     price: {type: Number, required: true},
     time: {type: Number, required: true},
     views: {type: Number, default: 0},
--- routes/products/products.js
+++ routes/products/products.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 const productModel = require("../../models/product");
+const categories = require("../../util/json/categories.json");
+
 
 router.get('/get', function(req, res, next) {
   productModel.find({}, (err, products) => {
@@ -40,20 +42,25 @@ router.get('/get/:id', function(req, res, next) {
 
 router.post('/post', function(req, res, next) {
   if(req.body.secret === process.env.PASSWORD) {
-    const product = new productModel({
-      title: req.body.title,
-      desc: req.body.desc,
-      price: req.body.price,
-      time: req.body.time,
-      pics: req.body.pics
-    });
-    product.save().then(result => {
-      return res.status(200).json({
-        "result": "Dodano produkt."
+    if(categories.categories.includes(req.body.category)) {
+      const product = new productModel({
+        title: req.body.title,
+        desc: req.body.desc,
+        category: req.body.category,
+        price: req.body.price,
+        time: req.body.time,
+        pics: req.body.pics
       });
-    }).catch(err => {
-      res.status(500).send(err);
-    })
+      product.save().then(result => {
+        return res.status(200).json({
+          "result": "Dodano produkt."
+        });
+      }).catch(err => {
+        res.status(500).send(err);
+      })
+    }else {
+      res.status(400).json({"result": "nie ma takiej kategorii" + "(dostepne: " + categories.categories + ")"})
+    }
   }else {
     res.status(401).json({"result": "unauthorized"});
   }
