935a2a5fdce34dda5e0fea84d00e38f11415b7f6
vjasieg <vjasieg@gmail.com>
2021-04-26 00:33:17 +0200

    pics posting and deleting fix

--- routes/products/pics/delete/pics_delete.js
+++ routes/products/pics/delete/pics_delete.js
@@ -2,12 +2,22 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 const productPicModel = require("../../../../models/productpic");
+const productModel = require("../../../../models/product")
 
 router.delete('', (req, res, next) => {
     if(!(req.body.secret === process.env.PASSWORD)) {
         res.status(401).json({"result": "unauthorized"});
         return next()
     }
+    productModel.findById({"_id": req.body.productID}).exec().then(product => {
+        product.pics.forEach((val, index) => {
+            console.log(val.replace("/files/", "").replace(".png", ""), req.body.picID)
+            if(req.body.picID === val.replace("/files/", "").replace(".png", "")) {
+                product.pics.splice(index, 1)
+                product.save()
+            }
+        })
+    })
     productPicModel.findById({"_id": req.body.picID}).exec().then(product => {
         fs.unlink("./static" + product.path, err => {})
         product.delete()
--- routes/products/pics/post/pics_post.js
+++ routes/products/pics/post/pics_post.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var fs = require('fs');
 var mongoose = require("mongoose")
 const productPicModel = require("../../../../models/productpic");
+const productModel = require("../../../../models/product")
 
 function upload(file, productid, order, id) {
     file.mv('./static/files/' + id + ".png", function(err) {
@@ -26,13 +27,17 @@ router.post('', (req, res, next) => {
 
     var id = mongoose.Types.ObjectId();
     file = req.files.file
+    productModel.findById({"_id": req.body.productID}).exec().then(product => {
+        product.pics.push("/files/" + id + ".png")
+        product.save()
+    })
     const pic = new productPicModel({
         "_id": id,
         "path": "/files/" + id + ".png",
         "productID": req.body.productID,
         "order": req.body.order
     });
-    upload(file, req.body.productid, req.body.order, id)
+    upload(file, req.body.productID, req.body.order, id)
     res.status(200).json({"result": "wyslano zdjecie", "pic_id": id})
 });
 
